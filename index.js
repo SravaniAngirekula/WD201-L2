0a4b15dfbf12efc235c13758d39d5fa4f861ffa7
SravaniAngirekula <105479052+SravaniAngirekula@users.noreply.github.com>
2022-11-13 13:45:20 +0530

    final

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-const http = require("http");
-const fs = require("fs");
-
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
+const http=require("http");
+const fs=require("fs");
+let homecontent="";
+let projectcontent="";
+let registrationcontent="";
 const args = require("minimist")(process.argv.slice(2));
-fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeContent = home.toString;
+fs.readFile("home.html",(err,home)=>{
+    if(err){ throw err};
+    homecontent=home.toString();
 });
-
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project.toString;
+fs.readFile("project.html",(err,project)=>{
+    if(err){
+        throw err;
+    }
+    projectcontent=project.toString();
 });
-
-fs.readFile("registration.html", (err, registration) => {
-  if (err) {
-    throw err;
-  }
-  registrationContent = registration.toString;
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err;
+    }
+    registrationcontent=registration.toString();
 });
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/registration":
-        response.write(registrationContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
+http.createServer((request,response)=>{
+    let url=request.url;
+    response.writeHead(200,{"content-type":"text/html"});
+    switch(url)
+    {
+        case "/project":
+            response.write(projectcontent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationcontent);
+            response.end();
+            break;
+        default:response.write(homecontent);
         response.end();
         break;
     }
-  })
-  .listen(args["port"]);
+}).listen(args["port"]);
